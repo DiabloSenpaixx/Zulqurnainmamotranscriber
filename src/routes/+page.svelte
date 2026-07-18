bc2204944fc5df478af7972ac701035228e11875
DiabloSenpaixx <diablosenpaiimvu@gmail.com>
2026-07-18 06:16:10 +0500

    Add custom dictionary rule for baqt to vaqt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,6 +92,7 @@
       text = text.replace(/\bwich\b/gi, match => match.charAt(0) === 'W' ? 'Bich' : 'bich');
       text = text.replace(/\btabla\b/gi, match => match.charAt(0) === 'T' ? 'Tavla' : 'tavla');
       text = text.replace(/\bbooda\b/gi, match => match.charAt(0) === 'B' ? 'Boota' : 'boota');
+      text = text.replace(/\bbaqt\b/gi, match => match.charAt(0) === 'B' ? 'Vaqt' : 'vaqt');
     }
     
     text = text.replace(/(^\s*|[.!?]\s+)(.)/g, (match, p1, p2) => p1 + p2.toUpperCase());
